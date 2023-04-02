b83487a84ef87939778ac9487bc9305939d0adb5
arsalyy <arsalmuhammad70@gmail.com>
2023-04-02 19:05:03 +0500

    Added Themes, MUI, Bootstrap, Global Styles

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,30 @@ import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import { PersistGate } from 'redux-persist/integration/react'
 import storage from 'redux-persist/lib/storage'
+import { Box, styled, ThemeProvider, CssBaseline } from '@material-ui/core'
 import { persistStore, persistReducer, createMigrate } from 'redux-persist'
-import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import App from './App'
+import './index.css'
+import './styles/global.scss'
 import rootReducer from '../src/reducers/rootReducer'
+import { getTheme } from 'utilities/theme'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+const AllAppWrapper = styled(Box)({
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  background: '#FFFFFF'
+})
+
+const AppContainer = styled(Box)({
+  maxWidth: '1600px',
+  height: '100%',
+  width: '100%'
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -30,11 +49,23 @@ export const store = createStore(persistedReducer)
 const persistor = persistStore(store)
 
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <AllAppWrapper>
+    <AppContainer>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={getTheme({})}>
+            <CssBaseline />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+              rel="stylesheet"
+            />
+            <App />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </AppContainer>
+  </AllAppWrapper>
 )
 
 // If you want to start measuring performance in your app, pass a function
